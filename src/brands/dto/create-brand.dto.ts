867238542f8624e3f9f08a0376d33678409e01c8
gabriel guzman <ggtpi0gabriel@gmail.com>
2026-01-09 17:14:02 -0300

    feat: Create Crud resources
    * brands
    * seed + ID

---
+++ src/brands/dto/create-brand.dto.ts
@@ -0,0 +1,13 @@
+import { IsString, MinLength } from "class-validator";
+
+
+
+
+
+
+export class CreateBrandDto {
+
+    @IsString()
+    @MinLength(1)
+    name: string;
+}
---
+++ src/brands/entities/brand.entity.ts
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+export class Brand {
+
+    id: string;
+    name: string;
+
+    createdAt: number;
+    updatedAt?: number;
+}
